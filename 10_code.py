f676cfe04368f26844769a5eb3337bcd606f2df9
Souvikmanna2005 <souvik2005110@gmail.com>
2026-03-21 10:33:38 +0530

    DAY 01

--- 10_code.py
+++ 10_code.py
@@ -1,3 +1,6 @@
+"""Implement a program that swaps the values of two
+variables."""
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print(f"Before Swaping: A={a}, B={b} ")
